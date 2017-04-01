b524b92a0ae9327cd0c0bd9714d8802f695c4e8b
williamkohler <bkohler4@gmail.com>
2017-04-01 18:06:33 -0400

    Initial commit

---
+++ src/AllFriends.java
@@ -0,0 +1,11 @@
+/**
+ * Created by williamkohler on 4/1/17.
+ */
+public class AllFriends {
+
+    public static void main(String[] args) {
+        System.out.println("Overpowered by funk");
+    }
+
+
+}
